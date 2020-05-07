efd496a21f80ac0e756052b564dc40b081564f8c
Forrest Li <forrest.li@kitware.com>
2020-05-07 15:10:19 -0400

    blah

---
+++ dist/itk/loadEmscriptenModuleBrowser.js
@@ -0,0 +1,44 @@
+import _typeof from "@babel/runtime/helpers/typeof";
+
+// Load the Emscripten module in the browser.
+//
+// If the browser supports WebAssembly, then use the path the the WebAssembly
+// wrapper module instead.
+//
+// If itkModulesPath is a relative Path, then resolve assuming we were called
+// from <itkModulesPath>/WebWorkers/, since modules are loaded by the web
+// workers.
+//
+//
+// itkModulesPath is usually taken from './itkConfig', but a different value
+// could be passed.
+//
+// modulesDirectory is one of "ImageIOs", "MeshIOs", or "Pipelines"
+//
+// moduleBaseName is the file name of the emscripten module without the ".js"
+// extension
+function loadEmscriptenModule(itkModulesPath, modulesDirectory, moduleBaseName) {
+  var prefix = itkModulesPath;
+
+  if (itkModulesPath[0] !== '/' && !itkModulesPath.startsWith('http')) {
+    prefix = '..';
+  }
+
+  var moduleScriptDir = prefix + '/' + modulesDirectory;
+
+  if ((typeof WebAssembly === "undefined" ? "undefined" : _typeof(WebAssembly)) === 'object' && typeof WebAssembly.Memory === 'function') {
+    var modulePath = moduleScriptDir + '/' + moduleBaseName + 'Wasm.js';
+    importScripts(modulePath);
+    var module = self[moduleBaseName]({
+      moduleScriptDir: moduleScriptDir
+    });
+    return module;
+  } else {
+    var _modulePath = moduleScriptDir + '/' + moduleBaseName + '.js';
+
+    importScripts(_modulePath);
+    return Module;
+  }
+}
+
+export default loadEmscriptenModule;
